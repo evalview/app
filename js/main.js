269dcbc926c2e3e9d9ba6ac034daed3026ad98c4
gwhagerty <gwhagerty@gmail.com>
2021-07-27 15:35:18 -0600

    Added videos to landing page.

--- js/main.js
+++ js/main.js
@@ -31,7 +31,20 @@ function main() {
       $(".navbar-collapse").collapse('hide');
     }
   });
-	
+
+  $(document).on("click", ".js-videoPoster", function (e) {
+    e.preventDefault();
+    var poster = $(this);
+    var wrapper = poster.closest(".js-videoWrapper");
+    videoPlay(wrapper);
+  });
+
+  function videoPlay(wrapper) {
+    var iframe = wrapper.find(".js-videoIframe");
+    var src = iframe.data("src");
+    wrapper.addClass("videoWrapperActive");
+    iframe.attr("src", src);
+  };
 	
     // Nivo Lightbox 
     $('.portfolio-item a').nivoLightbox({
