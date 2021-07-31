c5f43da1f7775c4ff152791e058f9e228bef477a
chuongtang <chuong.tang@aol.com>
2021-07-30 20:49:13 -0600

    fixed form submission and edit all styling. REAADY FOR DEPLY

--- mail/sendMsgFromWeb.js
+++ mail/sendMsgFromWeb.js
@@ -7,30 +7,30 @@ const resetFormAfterSend = () => {
   document.getElementById('message').value = '';
 }
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
+// form.addEventListener("submit", async (e) => {
+//   e.preventDefault();
 
-  const { name, email, message } = e.target;
+//   const { name, email, message } = e.target;
 
-  const msgBody = JSON.stringify({
-    senderName: name.value,
-    senderEmail: email.value,
-    message: message.value
-  });
-  console.log(msgBody);
+//   const msgBody = JSON.stringify({
+//     senderName: name.value,
+//     senderEmail: email.value,
+//     message: message.value
+//   });
+//   console.log(msgBody);
 
-  await Email.send({
-    Host: "smtp.elasticemail.com",
-    Username: "tdog.evolveu@gmail.com",
-    Password: "3B0B6C90CFA4E796D33BB116BC51B53D3993",
-    To: 'tdog.evolveu@gmail.com',
-    From: "tdog.evolveu@gmail.com",
-    Subject: "Message from Eval-view site",
-    Body: msgBody,
-  })
-    .then(() => {
-      alert("Thanks for contacting us. Your message has been successfully sent🚀");
-      resetFormAfterSend();
+//   await Email.send({
+//     Host: "smtp.elasticemail.com",
+//     Username: "tdog.evolveu@gmail.com",
+//     Password: "3B0B6C90CFA4E796D33BB116BC51B53D3993",
+//     To: 'tdog.evolveu@gmail.com',
+//     From: "tdog.evolveu@gmail.com",
+//     Subject: "Message from Eval-view site",
+//     Body: msgBody,
+//   })
+//     .then(() => {
+//       alert("Thanks for contacting us. Your message has been successfully sent🚀");
+//       resetFormAfterSend();
 
-    });
-})
+//     });
+// })
