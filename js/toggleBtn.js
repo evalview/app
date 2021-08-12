c29b5d2478a8046f320d4788219e96405a861783
chuongtang <chuong.tang@aol.com>
2021-08-11 20:45:35 -0600

    fixed the wrong url

---
+++ js/toggleBtn.js
@@ -0,0 +1,8 @@
+
+const toggleBtn = () => {
+  console.log("something here");
+  const appBtn = getElementById('appBtn');
+  setTimeout(() => {
+    appBtn.classList.add("btn btn-custom" );;
+  }, 2000);
+};
